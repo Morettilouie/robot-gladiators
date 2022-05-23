14e7d72998d08e133e6940b3a3a222f8c296d60a
Louie Moretti <morettilouie@gmail.com>
2022-05-23 17:39:05 -0500

    beginning random numbers

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,7 +13,6 @@ var playerMoney = 10;
 //console.log(playerName, playerAttack, playerHealth);
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-var enemyHealth = 10;
 var enemyAttack = 12;
 
 
@@ -35,14 +34,14 @@ var fight = function(enemyName) {
         if (confirmSkip) {
             window.alert(playerName + " has decided to skip this fight. Goodbye!");
             // subtract money from playerMoney for skipping
-            playerMoney = playerMoney - 10;
+            playerMoney = Math.max(0, playerMoney - 10);
             console.log("playerMoney", playerMoney);
             break;
         }
     } 
 
         // Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
-    enemyHealth = enemyHealth - playerAttack
+    enemyHealth = Math.max(0, enemyHealth - playerAttack)
 
     // Log a resulting message to the console so we know that it worked
     console.log(
@@ -63,7 +62,7 @@ var fight = function(enemyName) {
     }
 
     //Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable
-    playerHealth = playerHealth - enemyAttack
+    playerHealth = Math.max(0, playerHealth - enemyAttack)
 
     //Log a resulting message to the console so we know that it worked
     console.log(
@@ -100,7 +99,7 @@ var startGame = function() {
             // pick new enemy to fight based on the index of the enemyNames array
             var pickedEnemyName = enemyNames[i];
             // reset enemyHealth before starting new fight
-            enemyHealth = 50;
+            enemyHealth = Math.floor(Math.random() * 21) + 40;
             // use debugger to pause script from running and check what's going on at that moment in the code
             //debugger;
 
